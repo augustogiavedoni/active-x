d3e2fdd9e01d46f4ec744935b4ab8aafcf652f69
Augusto Giavedoni <augusto.giavedoni@gmail.com>
2025-04-09 16:13:32 -0300

    feat(dashboard): implement initial layout configuration

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,29 @@
+import { cookies } from "next/headers";
+
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/dashboard/app-sidebar";
+
+export default async function Layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const cookieStore = await cookies();
+  const defaultOpen = cookieStore.get("sidebar_state")?.value === "true";
+
+  return (
+    <SidebarProvider defaultOpen={defaultOpen}>
+      <AppSidebar variant="inset" />
+      <SidebarInset>
+        <main>
+          <SidebarTrigger />
+          {children}
+        </main>
+      </SidebarInset>
+    </SidebarProvider>
+  );
+}
---
+++ components/dashboard/app-sidebar.tsx
@@ -0,0 +1,93 @@
+"use client";
+
+import {
+  BookTextIcon,
+  Building2Icon,
+  DumbbellIcon,
+  LayoutDashboardIcon,
+  SettingsIcon,
+  UsersIcon,
+} from "lucide-react";
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import { NavUser } from "./nav-user";
+import { NavMain } from "./nav-main";
+import { NavSecondary } from "./nav-secondary";
+
+const data = {
+  user: {
+    name: "shadcn",
+    email: "m@example.com",
+    avatar: "/avatars/shadcn.jpg",
+  },
+  navMain: [
+    {
+      title: "Dashboard",
+      url: "#",
+      icon: LayoutDashboardIcon,
+    },
+    {
+      title: "Workouts",
+      url: "#",
+      icon: DumbbellIcon,
+    },
+    {
+      title: "Exercises",
+      url: "#",
+      icon: BookTextIcon,
+    },
+    {
+      title: "People",
+      url: "#",
+      icon: UsersIcon,
+    },
+    {
+      title: "Team",
+      url: "#",
+      icon: Building2Icon,
+    },
+  ],
+  navSecondary: [
+    {
+      title: "Settings",
+      url: "#",
+      icon: SettingsIcon,
+    },
+  ],
+};
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar collapsible="offcanvas" {...props}>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton
+              asChild
+              className="data-[slot=sidebar-menu-button]:!p-1.5"
+            >
+              <a href="#">
+                <span className="text-base font-semibold">Active-X</span>
+              </a>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        <NavMain items={data.navMain} />
+        <NavSecondary items={data.navSecondary} className="mt-auto" />
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={data.user} />
+      </SidebarFooter>
+    </Sidebar>
+  );
+}
